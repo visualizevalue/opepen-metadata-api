93189bb4d2ac0dd8a17f65e204754f912a0f7a25
jalil <jalil@1001.digital>
2023-12-26 12:17:16 +0100

    Fix rendering

--- app/Services/PageRenderer.ts
+++ app/Services/PageRenderer.ts
@@ -4,7 +4,10 @@ let browser: Browser
 
 export const getBrowser = async () => {
   if (! browser) {
-    browser = await puppeteer.launch({ headless: 'new' })
+    browser = await puppeteer.launch({
+      headless: 'new',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
   }
 
   return browser
