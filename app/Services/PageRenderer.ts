0bb3d6f1943a88e52615537fc57881c71a28fa2d
jalil <jalil@1001.digital>
2024-01-04 11:46:09 +0100

    Relax child processes

--- app/Services/PageRenderer.ts
+++ app/Services/PageRenderer.ts
@@ -4,13 +4,16 @@ import Logger from '@ioc:Adonis/Core/Logger'
 let browser: Browser
 
 export const newBrowser = async () => {
+  Logger.debug(`Loading new browser`)
   browser = await puppeteer.launch({
     headless: 'new',
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: ['--single-process', '--no-zygote', '--no-sandbox', '--disable-setuid-sandbox'],
   })
+  Logger.debug(`New browser loaded`)
 }
 
 export const getBrowser = async () => {
+  Logger.debug(`Getting browser`)
   if (! browser) {
     await newBrowser()
   }
@@ -19,6 +22,7 @@ export const getBrowser = async () => {
 }
 
 export const renderPage = async (url: string, dimension: number = 960) => {
+  Logger.debug(`Trying to render page (${url})`)
   const browser = await getBrowser()
   const page = await browser.newPage()
 
@@ -28,13 +32,14 @@ export const renderPage = async (url: string, dimension: number = 960) => {
     await page.waitForFunction("RENDERED === true", {
       timeout: 1000,
     })
+    Logger.debug(`Rendered page (${url})`)
   } catch (e) {}
 
   const image = await page.screenshot({});
-  Logger.debug('Screenshot captured')
+  Logger.debug(`Screenshot captured (${url})`)
 
   await page.close()
-  Logger.debug('Page closed')
+  Logger.debug(`Page closed (${url})`)
 
   return image
 }
